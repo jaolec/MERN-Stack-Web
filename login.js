80910f030d6c176becf04468558343c4c2fb01dc
Nick-Bonilla-0915 <ni865929@ucf.edu>
2026-04-04 15:06:25 -0400

    Adding Functions
    Added multiple functions that will make the website work. Also added HTML for the create user section.

--- login.js
+++ login.js
@@ -1,7 +1,40 @@
+//This function should check a few conditions. 
+//Is the login valid, is the account admin or tenant
+//Then, it should change the CSS to show the correct page
+//(Change either #tenant or #admin to display:block or whatever display you think works)
 function logIn(){
     
 }
 
+//This function will change the display of #createUser to show the create user menu
 function createUser(){
+    
+}
+
+function tenManage(){
+
+}
+
+function batManage(){
+
+}
+
+function mainManage(){
+
+}
+
+function checkProp(){
+
+}
+
+function checkPay(){
+
+}
+
+function checkMain(){
+
+}
+
+function viewProf(){
 
 }
